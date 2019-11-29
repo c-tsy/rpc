4982793226a90f406e1b28c5447e2a79f1534118
castleiMac <castle@tansuyun.cn>
2019-11-29 15:48:04 +0800

    topic支持_命名

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ export enum DataType {
 }
 export function checkTopic(topic: string): string {
     if ('string' != typeof topic) { throw 'ErrorTopic' }
-    if (/^[A-Za-z0-9][A-Za-z0-9\+\/\$\#]{0,}$/g.test(topic)) {
+    if (/^[A-Za-z0-9][A-Za-z0-9\+\/\$\#_]{0,}$/g.test(topic)) {
         return '^' + topic.replace(/\$/g, '[A-Za-z0-9]').replace(/\+/g, '[A-Za-z0-9]{1,}').replace(/\#/g, '[A-Za-z0-9\\/]') + '$'
     }
     throw 'ErrorTopic'
